45d04de4fbd7e983cf65727492b2a38a83bcada8
mjehrhart <97703291+mjehrhart@users.noreply.github.com>
2022-03-08 21:38:05 -0800

    updated

--- Formula/ff.rb
+++ Formula/ff.rb
@@ -1,13 +1,13 @@
-class Ff < Formula
- desc "Fast Find is a cli written in rust using rayon parrellism for super fast results."
- homepage "https://github.com/mjehrhart/homebrew-ff"
- url "https://github.com/mjehrhart/ffind/releases/download/release/ffind.tar.gz"
- version "1.0.0"
- sha256 "3a027d0ff8be688702f0c7f73c79ec08d36d2d8796e7fee65986d7115f4d034c"
- license "BSD 2-Clause License"
+ class ff < Formula
+  desc "Fast Find is a cli written in rust using rayon parrellism for super fast results."
+  homepage "https://github.com/mjehrhart/homebrew-ff"
+  url "https://github.com/mjehrhart/ffind/releases/download/release/ff.tar.gz"
+  version "1.0.0"
+  sha256 "6cf3ad51fbecee4dc49809776d5361fc97674dd612898a66d74956955f373c6f"
+  license "BSD 2-Clause License"
 
- def install
-	 bin.install "Ff"
- end
+  def install
+    bin.install "ff"
+  end
 
 end
